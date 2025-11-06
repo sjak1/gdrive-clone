379fb97145b5650e3430407b4249fe59e2d15e28
Sai Jyothir Aditya Kantipudi <106384725+0xAdityaKsj@users.noreply.github.com>
2025-11-06 23:13:42 +0530

    Database Added

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,13 @@ import { env } from "~/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
+  dialect: "singlestore",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    host: env.SINGLESTORE_HOST,
+    port: parseInt(env.SINGLESTORE_PORT),
+    user: env.SINGLESTORE_USERNAME,
+    password: env.SINGLESTORE_PASSWORD,
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
   },
-  tablesFilter: ["drive-tutorial_*"],
 } satisfies Config;
